1dddfe0ea9ca4611ca96dfb45ff20763e04a7efa
Manuela Alecio <manuelaalecio.dev@gmail.com>
2025-02-27 17:57:22 -0300

    feat: submit function

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 import { openingHours } from "../../utils/opening-hours.js";
+import { hoursClick } from "./hours-click.js";
 
 const hours = document.querySelector("#hours");
 
@@ -30,6 +31,8 @@ export function hoursLoad({ date }) {
 
     hours.appendChild(li);
   });
+
+  hoursClick();
 }
 
 function hoursHeaderAdd(title) {
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.querySelector("#client");
 const selectedDate = document.querySelector("#date");
 
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -11,5 +12,32 @@ selectedDate.min = inputToday;
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  console.log("Enviado");
+  try {
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente");
+    }
+
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Selecione uma hora");
+    }
+
+    const [hour] = hourSelected.innerText.split(":");
+
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    const id = new Date().getTime();
+
+    return {
+      id,
+      name,
+      when,
+    };
+  } catch (error) {
+    alert("Não foi possivel criar o agendamento");
+    console.error(error);
+  }
 };
