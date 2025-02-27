c6c181d9e7ed2b6ddbf54ae2048708278c694ca5
Manuela Alecio <manuelaalecio.dev@gmail.com>
2025-02-26 21:03:23 -0300

    feat: adding period

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -19,6 +19,23 @@ export function hoursLoad({ date }) {
     li.classList.add(available ? "hour-available" : "hour-unavailable");
 
     li.textContent = hour;
+
+    if (hour === "9:00") {
+      hoursHeaderAdd("Manhã");
+    } else if (hour === "13:00") {
+      hoursHeaderAdd("Tarde");
+    } else if (hour === "18:00") {
+      hoursHeaderAdd("Noite");
+    }
+
     hours.appendChild(li);
   });
+}
+
+function hoursHeaderAdd(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hours.append(header);
 }
