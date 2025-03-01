149c18dfecf4e2b0e78a1cc091853435791721c2
Manuela Alecio <manuelaalecio.dev@gmail.com>
2025-02-28 21:14:31 -0300

    feat: showing api informations

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,15 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleShow } from "./show.js";
 
 const selectedDate = document.querySelector("#date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   const date = selectedDate.value;
 
-  hoursLoad({ date });
+  const dailySChedules = await scheduleFetchByDay({ date });
+
+  scheduleShow({ dailySChedules });
+
+  hoursLoad({ date, dailySChedules });
 }
